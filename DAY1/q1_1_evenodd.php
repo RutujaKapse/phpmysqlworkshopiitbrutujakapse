ee5034535a61f43139c93a14c7b483205a439bae
Rutuja Kapse <kapserutya18it@student.mes.ac.in>
2020-10-05 22:28:23 +0530

    First Commit

---
+++ DAY1/q1_1_evenodd.php
@@ -0,0 +1,47 @@
+<?php  
+$number=11;  
+if($number%2==0)  
+{  
+ echo "$number is Even Number";   
+}  
+else  
+{  
+ echo "$number is Odd Number";  
+}   
+?>  
+<br>
+<?php  
+$number=33;  
+if($number%2==0)  
+{  
+ echo "$number is Even Number";   
+}  
+else  
+{  
+ echo "$number is Odd Number";  
+}   
+?>  
+<br>
+<?php  
+$number=52;  
+if($number%2==0)  
+{  
+ echo "$number is Even Number";   
+}  
+else  
+{  
+ echo "$number is Odd Number";  
+}   
+?>  
+<br>
+<?php  
+$number=88;  
+if($number%2==0)  
+{  
+ echo "$number is Even Number";   
+}  
+else  
+{  
+ echo "$number is Odd Number";  
+}   
+?>  
